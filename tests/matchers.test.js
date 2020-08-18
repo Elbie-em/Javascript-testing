4c3b0f4008190b573274bc11640ad45f343a570e
Elbie-em <emoonga1993@gmail.com>
2020-08-18 22:11:10 +0200

    Test matchers for numbers, string and arrays

--- tests/matchers.test.js
+++ tests/matchers.test.js
@@ -36,12 +36,47 @@ test('zero', () => {
 // Most ways of comparing numbers have matcher equivalents.
 test('two plus two', () => {
     const val = 2 + 2;
-    expect(value).toBeGreaterThan(3);
-    expect(value).toBeGreaterThanOrEqual(3.5);
-    expect(value).toBeLessThan(5);
-    expect(value).toBeLessThanOrEqual(4.5);
+    expect(val).toBeGreaterThan(3);
+    expect(val).toBeGreaterThanOrEqual(3.5);
+    expect(val).toBeLessThan(5);
+    expect(val).toBeLessThanOrEqual(4.5);
 
     // toBe and toEqual are equivalent for numbers
-    expect(value).toBe(4);
-    expect(value).toEqual(4);
-})
+    expect(val).toBe(4);
+    expect(val).toEqual(4);
+});
+
+// For floating point equality, use toBeCloseTo instead of toEqual, because you don't want a test to depend on a tiny rounding error.
+test('addition of floating point numbers', () => {
+    const val = 0.1 + 0.2;
+    //expect(value).toBe(0.3);           This won't work
+    expect(val).toBeCloseTo(0.3); // This works.
+});
+
+// Strings
+// You can check strings against regular expressions with toMatch method:
+test('there is no U in college',() => {
+    expect('College').not.toMatch(/U/);
+});
+
+test('there is city in Velocity', () => {
+    const word = 'Velocity';
+    expect(word).toMatch(/city/);
+});
+
+// Arrays and iterables
+// You can check if an array or iterable object contains a particular item using the toContain method:
+
+const shoppingCart = [
+    'milk',
+    'bread',
+    'butter',
+    'cereal',
+    'honey',
+    'sugar'
+]
+
+test('the shopping list has cereal or sugar in it', () => {
+    expect(shoppingCart).toContain('cereal');
+    expect(new Set(shoppingCart)).toContain('sugar');
+});
