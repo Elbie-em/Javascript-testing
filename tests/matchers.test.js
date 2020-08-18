d5f01cf203511a019dbfcea25875ea371d25e2c2
Elbie-em <emoonga1993@gmail.com>
2020-08-18 21:46:26 +0200

    Testing for null

--- tests/matchers.test.js
+++ tests/matchers.test.js
@@ -3,7 +3,22 @@ test('two plus two is four', () => {
 });
 
 test('object assignment', () => {
-    const data = {one: 1};
+    const data = { one: 1 };
     data['two'] = 2;
-    expect(data).toEqual({one: 1,two: 2});
+    expect(data).toEqual({ one: 1, two: 2 });
+});
+
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
+
+test('null', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
 });
